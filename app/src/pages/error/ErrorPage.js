d7b78d074c125fb077cf1c5fac3a06b6632c745e
ZlLow <zhanlonglow@outlook.com>
2023-02-07 01:35:20 +0800

    Moved Images to src & added Basic Error Page

--- app/src/pages/error/ErrorPage.js
+++ app/src/pages/error/ErrorPage.js
@@ -1,6 +1,16 @@
+import React from "react";
+import "../../assets/css/ErrorPage.css";
+import Stack from "react-bootstrap/Stack";
+
 export default function ErrorPage() {
     document.title = "404 Error Page";
     return (
-        <div>test</div>
+        <div className="bg-image">
+            <Stack gap={3} className="mx-auto">
+                <div></div>
+                <div></div>
+                <h1 className="d-flex justify-content-center align-items-center">404 Error</h1>
+            </Stack>
+        </div>
     )
 }
--- app/src/pages/navigation/navbar/Navbar.js
+++ app/src/pages/navigation/navbar/Navbar.js
@@ -1,53 +1,62 @@
-import {Outlet, Link} from "react-router-dom";
+import {Outlet, Link, useLocation} from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import {LinkContainer} from "react-router-bootstrap";
 import {Button} from "react-bootstrap";
-import "./Navbar.css";
+import "../../../assets/css/Navbar.css";
 
-function nav() {
+const withoutSidebarRoutes = ["/login"];
+
+export default function NavigationBar() {
+    const {pathname} = useLocation();
+
+    const noSideBars = withoutSidebarRoutes.some((item) => pathname.includes(item));
+
+    const navbarToggle =
+        <>
+        <Navbar.Toggle aria-controls="navbar-nav"/>
+        <Navbar.Collapse id="navbar-nav">
+            <Nav className="ms-auto">
+                <LinkContainer to="/">
+                    <Nav.Link className="mx-2">Workout</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                    <Nav.Link className="mx-2">Exercise Facilities</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/register">
+                    <Nav.Link className="mx-2">Community</Nav.Link>
+                </LinkContainer>
+            </Nav>
+            <Nav className="ms-auto">
+                <LinkContainer to="/login">
+                    <Nav.Link>
+                        <Button variant="outline-primary" className="text-light">Sign in</Button> {' '}
+                    </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/register">
+                    <Nav.Link>
+                        <Button variant="outline-secondary" className="text-light">Sign Up</Button>{' '}
+                    </Nav.Link>
+                </LinkContainer>
+            </Nav>
+        </Navbar.Collapse>
+    </>
+
     return (
     <>
         <Navbar collapseOnSelect bg="darkblue" expand="lg" variant="dark">
-            <Container fluid className="mx-4">
+            <Container fluid className="mx-5">
                 <LinkContainer to="/">
                     <Navbar.Brand>
                         <b>Train</b>
                         Together
                     </Navbar.Brand>
                 </LinkContainer>
-                <Navbar.Toggle aria-controls="navbar-nav"/>
-                <Navbar.Collapse id="navbar-nav">
-                    <Nav className="ms-auto">
-                        <LinkContainer to="/">
-                            <Nav.Link className="mx-2">Workout</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/login">
-                            <Nav.Link className="mx-2">Exercise Facilities</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/register">
-                            <Nav.Link className="mx-2">Community</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                    <Nav className="ms-auto">
-                        <LinkContainer to="/login">
-                            <Nav.Link>
-                                <Button variant="outline-primary" className="text-light">Sign in</Button> {' '}
-                            </Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/register">
-                            <Nav.Link>
-                                <Button variant="outline-secondary" className="text-light">Sign Up</Button>{' '}
-                            </Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
+                {noSideBars ? null : navbarToggle}
             </Container>
         </Navbar>
         <Outlet/>
     </>
     )
 }
-
-export default nav;
