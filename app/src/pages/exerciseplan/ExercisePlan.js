7032975794b7c9dd557b1c0fb0a708479d81936d
Lee Yen Foong Ernest <leyefoer@gmail.com>
2023-03-07 00:24:20 +0800

    edited to fit the new css

--- app/src/pages/exerciseplan/ExercisePlan.js
+++ app/src/pages/exerciseplan/ExercisePlan.js
@@ -8,8 +8,8 @@ export default function ExercisePlan() {
 
     return (
         <div className={background.default}>
-            <div className="Container">
-                <div className="Title">
+            <div className="exercise-plan-container">
+                <div className="exercise-plan-title">
                     <h1>Exercise Plans</h1>
                     <h2>Arms Day</h2>
                 </div>
