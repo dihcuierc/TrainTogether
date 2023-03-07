ce3929d58c5c95746accf116eec4fd419965723d
Lee Yen Foong Ernest <leyefoer@gmail.com>
2023-03-07 12:47:33 +0800

    Did fitness goals in profile page but not dynamic yet

--- app/src/pages/components/dashboard/goals/ViewGoals.js
+++ app/src/pages/components/dashboard/goals/ViewGoals.js
@@ -4,34 +4,37 @@ import "./ViewGoals.css";
 import React, { useState } from "react";
 
 export default function ViewGoals() {
+    const [goals, setGoals] = useState([]);
+
+    //create function to fetch goals from database
+
     const [refreshCount, setRefreshCount] = useState(0);
 
     function handleGoalDoneClick() {
       setRefreshCount(refreshCount + 1);
     }
+    // do a map for the goals instead of hard code
     return (
-        <>
-            <Card className="bg-dark opacity-75">
-                    <Card.Title className="text-white"> Fitness Goals </Card.Title>
-                    <Card.Body>
-                        <Card.Body className="fitness-goal">
-                            <Stack direction="horizontal">
-                                <Stack>
-                                    <p className="goal-to-achieve">Weight Goal</p>
-                                    <p className="current-standing">Current Weight</p>
-                                </Stack>
-                                <Stack>
-                                    <p className="goal-to-achieve">50kg</p>
-                                    <p className="current-standing">60kg</p>
-                                </Stack>
-                                <Stack>
-                                    <p className="goal-date">date</p>
-                                    <button className="goal-done" onClick={handleGoalDoneClick}>Done</button>
-                                </Stack>
-                            </Stack>
-                        </Card.Body>
-                    </Card.Body>
-            </Card>
-        </>
+        <Card className="bg-dark opacity-75">
+            <Card.Title className="text-white"> Fitness Goals </Card.Title>
+            <Card.Body>
+                <Card.Body className="fitness-goal">
+                    <Stack direction="horizontal">
+                        <Stack>
+                            <p className="goal-to-achieve">Weight Goal</p>
+                            <p className="current-standing">Current Weight</p>
+                        </Stack>
+                        <Stack>
+                            <p className="goal-to-achieve">50kg</p>
+                            <p className="current-standing">60kg</p>
+                        </Stack>
+                        <Stack>
+                            <p className="goal-date">date</p>
+                            <button className="goal-done" onClick={handleGoalDoneClick}>Done</button>
+                        </Stack>
+                    </Stack>
+                </Card.Body>
+            </Card.Body>
+        </Card>
     )
 }
