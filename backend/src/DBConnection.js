637e4303707c7b78362dda00682cae22dc7d16b1
Kai <kaijie00@hotmail.com>
2023-03-14 11:23:17 +0800

    Added User attributes

--- backend/src/DBConnection.js
+++ backend/src/DBConnection.js
@@ -1,4 +1,6 @@
-import {collection, onSnapshot, getFirestore, addDoc, deleteDoc, doc, query, where} from "firebase/firestore";
+import {collection, onSnapshot, getFirestore, addDoc, deleteDoc, 
+    doc, query, where, orderBy, serverTimestamp
+} from "firebase/firestore";
 
 function collectData() {
     //init service
@@ -7,10 +9,10 @@ function collectData() {
     // User User collection ref
     const UserRef = collection(db, 'User')
 
-    // User query -> Maybe can pass in a string to search for a particular friend
+    // User query -> Maybe can pass in a string to search for a particular "friend"
     // then return q as the friend result details
-    const q = query(UserRef, where("User", "==", "friend"))
-
+    const q = query(UserRef, where("User", "==", "friend"), orderBy('createdAt'))
+    
     // real time User collection data
     onSnapshot(UserRef, (snapshot) => {
         let User = []
@@ -30,7 +32,16 @@ function addData() {
 
         addDoc(UserRef, {
             //all the attributes
-            //<attribute> : addUser.<input_field>.value
+            password: addUser.password.value,
+            firstName: addUser.firstName.value,
+            lastName: addUser.lastName.value,
+            username: addUser.username.value,
+            email: addUser.email.value,
+            profile: addUser.profile.value,
+            image: addUser.image.value,
+            mobile: addUser.mobile.value,
+            //goals, Reviews, friends can add separately
+            createdAt: serverTimestamp()
         })
         .then(() => {
             addUser.reset()
@@ -43,7 +54,7 @@ function deleteData() {
     const deleteUser = document.querySelector('.delete')
     deleteUser.addEventListener('submit', (e) => {
         e.preventDefault
-
+        // require the id of the document to delete
         const UserRef = doc(db, 'User', deleteUser.id.value)
 
         deleteDoc(UserRef)
