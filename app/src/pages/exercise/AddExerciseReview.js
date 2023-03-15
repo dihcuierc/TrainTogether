ff6783e76881c2b3dd9373491bf94b43d9ef5490
Lee Yen Foong Ernest <leyefoer@gmail.com>
2023-03-16 02:41:10 +0800

    added review rating bar that is dynamic

--- app/src/pages/exercise/AddExerciseReview.js
+++ app/src/pages/exercise/AddExerciseReview.js
@@ -33,15 +33,17 @@ export default function AddExerciseReview(prop) {
             return;
         }
 
+        const currentDate = new Date();
+        const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
         const newReview = {
-            id: exerciseReviews.length + 1,
+            id: exercise.id,
             "path": "/images/ExerciseImages/InclineBicepCurl.gif",
             "alt": "Incline Bicep Curl",
-            "exercise-group-id": 4,
+            "exercise-group-id": exercise["exercise-group-id"],
             "name": "Sugma",
             "rating": value,
             "text": review,
-            "date":"10/3/2023"
+            "date": formattedDate
         }
      
         exerciseReviews.push(newReview);
@@ -58,49 +60,49 @@ export default function AddExerciseReview(prop) {
                     <Card.Title className='exercise-review-title'>
                         <p style={{color:'white'}}>Add Review For Exercise</p>
                     </Card.Title>
-                    <Stack direction="horizontal" gap={3}>
+                    <Stack direction="horizontal" gap={3} align-items-start>
                         <Card.Body className='review-exercise-exercise'>
                             <p style={{color:'white'}}>This workout is under your plan:</p>
                             <p className="exercise-review-type">Arms Day</p>
                             <img className="review-exercise-video" src={exercise.path} alt={exercise.alt} />
                         </Card.Body>
                         
-                            <Card.Body className='exercise-review-rating-text'>
-                                <Card.Title>
-                                    <p style={{color:'white'}}>Your Rating</p>
-                                </Card.Title>
+                        <Card.Body className='exercise-review-rating-text'>
+                            <Card.Title>
+                                <p style={{color:'white'}}>Your Rating</p>
+                            </Card.Title>
+                            <Stack direction="vertical" gap={3}>
+                                <Card.Body className='exercise-review-star'>
+                                    <Rating className="exercise-rating-star"
+                                        name="half-rating" 
+                                        defaultValue={0} 
+                                        
+                                        onChange={(event, newValue) => {
+                                            setValue(newValue);
+                                            }}
+                                        onChangeActive={(event, newHover) => {
+                                            setHover(newHover);
+                                            }}
+                                        iconEmpty='white'
+                                        />
+                                </Card.Body>
+                                <Card.Body className='exercise-review-text'>
                                 <Stack direction="vertical" gap={3}>
-                                    <Card.Body className='exercise-review-star'>
-                                        <Rating className="exercise-rating-star"
-                                            name="half-rating" 
-                                            defaultValue={0} 
-                                            
-                                            onChange={(event, newValue) => {
-                                                setValue(newValue);
-                                              }}
-                                            onChangeActive={(event, newHover) => {
-                                                setHover(newHover);
-                                              }}
-                                            iconEmpty='white'
-                                            />
-                                    </Card.Body>
-                                    <Card.Body className='exercise-review-text'>
-                                    <Stack direction="vertical" gap={3}>
-                                        <textarea
-                                            id="review-text"
-                                            name="review-text"
-                                            className="multiline-text exercise-review-words"
-                                            value={review}
-                                            onChange={handleReviewChange}
-                                            rows='5'
-                                            
-                                        ></textarea>
-                                        <button className="submit-button" onClick={handleSubmit}>Submit</button>
-                                        <Toaster />
-                                        </Stack>
-                                    </Card.Body>
-                                </Stack>
-                            </Card.Body>
+                                    <textarea
+                                        id="review-text"
+                                        name="review-text"
+                                        className="multiline-text exercise-review-words"
+                                        value={review}
+                                        onChange={handleReviewChange}
+                                        rows='5'
+                                        
+                                    ></textarea>
+                                    <button className="submit-button" onClick={handleSubmit}>Submit</button>
+                                    <Toaster />
+                                    </Stack>
+                                </Card.Body>
+                            </Stack>
+                        </Card.Body>
                         
                     </Stack>
                 </Card>
--- app/src/pages/exercise/ExerciseReview.js
+++ app/src/pages/exercise/ExerciseReview.js
@@ -8,6 +8,8 @@ import Card from "react-bootstrap/Card";
 import Stack from "react-bootstrap/Stack";
 import Rating from '@mui/material/Rating';
 import exerciseReviews from "../../data/exerciseReviews.json";
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
 
 
 export default function ExerciseReview() {
@@ -18,24 +20,44 @@ export default function ExerciseReview() {
     const navigate = useNavigate();
     const [reviews, setReviews] = useState([]);
     const [averageRating, setAverageRating] = useState(0);
+    const [ratingCounts, setRatingCounts] = useState([0, 0, 0, 0, 0]);
 
     const addReview = () => {
         navigate('add');
     };
 
     useEffect(() => {
-        const filteredReviews = exerciseReviews.filter((review) => review["exercise-group-id"] === exercise["exercise-group-id"]);
+        const filteredReviews = exerciseReviews.filter((review) => review["id"] === exercise["id"]);
         setReviews(filteredReviews);
 
         const totalRating = filteredReviews.reduce((acc, filteredReviews) => {
             return acc + filteredReviews.rating;
           }, 0);
-      
+        
           const averageRating = totalRating / filteredReviews.length;
           setAverageRating(averageRating);
+        
+        const counts = [0, 0, 0, 0, 0];
+        filteredReviews.forEach((review) => {
+            counts[5 - review.rating] += 1;
+        });
+        setRatingCounts(counts);
+          
       }, [exercise]);
 
 
+      const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+        height: 15,
+        width:300,
+        borderRadius: 5,
+        [`&.${linearProgressClasses.colorPrimary}`]: {
+          backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+        },
+        [`& .${linearProgressClasses.bar}`]: {
+          borderRadius: 5,
+          backgroundColor: theme.palette.mode === 'light' ? '#56AF55' : '#56AF55',
+        },
+      }));
     return(
         <div className={background.default}>
             <Container className='all-exercise-review-container'>
@@ -53,10 +75,21 @@ export default function ExerciseReview() {
                             <img className="exercise-video-review" src={exercise.path} alt={exercise.alt} style={{display: 'block', margin: 'auto'}}/>
                             <p style={{ textAlign: 'center' }}>Overall Rating For This Exercise:</p>
                             <Stack direction="horizontal" gap={2} style={{ display: 'flex', justifyContent: 'center' }}>
-                                <Rating name="half-rating-read" value={averageRating} precision={0.5} size="large" iconEmpty='white' readOnly />
+                                <Rating name="half-rating-read" value={averageRating} precision={0.1} size="large" iconEmpty='white' readOnly />
                                 <h3 style={{paddingTop:'10px'}}>{averageRating.toFixed(1)}</h3>
                             </Stack>
-                            <p>Percentages</p>
+                            <br></br>
+                            <div className='percentage'>
+                                <Stack direction="vertical" gap={4}>
+                                    {ratingCounts.map((rating_score, index) => (
+                                        <Stack direction="horizontal" gap={3} style={{height:"5px"}}>
+                                        <p style={{height:"7px", width: "30px"}}>{Math.round(rating_score/reviews.length * 100)}%</p>
+                                        <BorderLinearProgress variant="determinate" value={rating_score/reviews.length * 100} />
+                                        <p style={{height:"7px"}}>{index+1}</p>
+                                        </Stack>
+                                    ))}
+                                </Stack>
+                            </div>
                         </div>
                         <div className='all-exercise-reviews'>
                             <Stack direction="vertical" gap={3}>
