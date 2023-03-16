c3c809f5554b2e1a1d7c8173308063fa87d15e02
Qwerty <zhanlonglow@outlook.com>
2023-03-17 00:03:27 +0800

    Added verification, signIn & etc

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,43 +1,120 @@
-import {getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    sendEmailVerification,
+    updatePassword,
+    updateProfile ,
+    sendPasswordResetEmail} from "firebase/auth";
 import {firebase} from "../firebaseConfig";
+import {Status} from "../errors/errorHandler";
 
-function decodingBasicAuth(auth) {
-    const details = auth.split(' ');
-    let buf = Buffer.from(details[1], "base64");
-    const plain = buf.toString();
-    const creds = plain.split(":");
-    let email = creds[0];
-    let password = creds[1];
-    return {email, password};
+let auth = getAuth(firebase);
+
+async function signUp(b64auth) {
+    let {authenticated, email, password, err} = authentication(b64auth);
+    if (err)
+        return {authenticated, err};
+    return await createUserWithEmailAndPassword(auth,email,password)
+        .then(async (userCredential) => {
+            const user = userCredential.user;
+            const accessToken = await user.getIdToken();
+            authenticated = true;
+            return {authenticated, accessToken};
+        })
+        .catch((err) => {
+            err.code = Status.Forbidden;
+            return {authenticated,err};
+        });
 }
 
-async function signUp(auth,res) {
-    const authMethod = getAuth(firebase);
-    let authenticated = false;
-    const {email, password} = decodingBasicAuth(auth);
-    if (!email|| !password)
-        return authenticated;
-    return await createUserWithEmailAndPassword(authMethod,email,password)
+async function logIn(b64auth) {
+    let {authenticated, email, password, err} = authentication(b64auth);
+    if (err)
+        return {authenticated, err};
+    return await signInWithEmailAndPassword(auth,email,password)
         .then(async (userCredential) => {
             const user = userCredential.user;
             const accessToken = await user.getIdToken();
             const verified = user.emailVerified;
             authenticated = true;
             return {authenticated, accessToken, verified};
-        })
-        .catch((err) => {
-            console.log(err);
-            return {authenticated,err};
-        })
+    }).catch((err) => {
+        err.code = Status.Forbidden;
+        return {authenticated, err};
+    })
 
 }
 
-function signIn() {
+ async function logOut(accessToken) {
+     let loggedOut = false;
+     return await signOut(accessToken).then(() => {
+         loggedOut = true;
+         return {loggedOut};
+     }).catch((err) => {
+         return {loggedOut, err}
+     });
+ }
 
+ async function verify() {
+    let verified = false;
+    let user = auth.currentUser;
+    if (!user) {
+        let err = new Error("No Current User is Logged In");
+        err.code = Status.NotFound;
+        return {verified, err};
+    }
+    return await sendEmailVerification(user)
+        .then(() => {
+            console.log("Email Sent");
+            verified = true;
+            return {verified};
+        }).catch((err) => {
+            err.code = Status.BadRequest;
+            return {verified, err};
+    });
+ }
+
+ async function updatePass() {
+
+ }
+
+ async function updateUser() {
+
+ }
+
+function decodingBasicAuth(b64auth) {
+    try {
+        const details = b64auth.split(' ');
+        let buf = Buffer.from(details[1], "base64");
+        const plain = buf.toString();
+        const creds = plain.split(":");
+        let email = creds[0];
+        let password = creds[1];
+        return {email, password};
+    } catch (err) {
+        let error = new Error("Wrong Authentication Method");
+        error.code = Status.BadRequest;
+        error.stack = err.stack;
+        throw error;
+    }
 }
 
-function signOut() {
-
+function authentication(b64auth) {
+    let authenticated = false;
+    try {
+        let {email, password} = decodingBasicAuth(b64auth);
+        if (!email || !password) {
+            let err = new Error("Authorization Header is empty");
+            err.code = Status.BadRequest;
+            return {authenticated, err};
+        }
+        return {authenticated, email, password}
+    } catch (err) {
+        return {authenticated, err};
+    }
 }
 
-export {signUp, signIn, signOut};
+
+
+export {signUp, logIn, logOut, verify};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
-import {initializeApp} from "firebase/app";
 
 import authRoutes from "./routes/authentication";
-import {firebase} from "./firebaseConfig";
 import {collectData} from "./DBConnection";
+import {errorHandler} from "./errors/errorHandler";
 
 
 const app = express();
@@ -12,8 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/users",authRoutes);
+app.use(errorHandler);
 
-app.listen(80, () => {
-    console.log(`Server running on localhost:80`);
+app.listen(8080, () => {
+    console.log(`Server running on localhost:8080`);
     collectData();
 })
