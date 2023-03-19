fd76901092e568ed72d45d11278eff666ebe4bd8
Qwerty <zhanlonglow@outlook.com>
2023-03-20 05:35:12 +0800

    Added Map API with Marker

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Routing from "./navigation/routing";
+import Routing from "./pages/components/navigation/routing";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- app/src/pages/facilities/Facilities.js
+++ app/src/pages/facilities/Facilities.js
@@ -1,8 +1,35 @@
-import Map from "../components/map/Map"
+import GoogleMap from "../components/map/GoogleMap";
+
+import parkData from "../../data/park-facilities-geojson.json"
+import Marker from "../components/map/Marker";
+
 export default function Facilities() {
+    const defaultProps = {
+        center: {
+            lat: 1.44883613021403,
+            lng: 103.771463555332005
+        },
+        zoom: 10
+    };
+    const places = parkData.features;
+    console.log(places)
     return (
         <>
-            <Map/>
+            <GoogleMap
+                bootstrapURLKeys={{
+                    key: "",
+            }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+            >
+                {places.map((place) => (
+                    <Marker
+                        key={place.properties.Name}
+                        lng={place.geometry.coordinates[0]}
+                        lat={place.geometry.coordinates[1]}
+                        place={place}/>
+                ))}
+            </GoogleMap>
         </>
     )
 }
