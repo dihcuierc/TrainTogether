f4cdc7da419bf311139c5b936c50b8384d220d3f
Lee Yen Foong Ernest <leyefoer@gmail.com>
2023-03-20 21:42:59 +0800

    BMI

--- app/src/pages/components/dashboard/ViewBMI.js
+++ app/src/pages/components/dashboard/ViewBMI.js
@@ -1,81 +1,115 @@
 import Card from "react-bootstrap/Card";
 import './ViewBMI.css';
 import Stack from "react-bootstrap/Stack";
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import { LinearProgress, makeStyles } from "@mui/material";
 
 export default function BMI() {
-    const [selected, setSelected] = useState('This Week');
 
-  const handleSelect = (eventKey) => {
-    setSelected(eventKey);
-  };
+    const [height, setHeight] = useState('0');
+    const [weight, setWeight] = useState('0');
+    const [bmi, setBMI] = useState(0);
+
+
+
+    useEffect(() => {
+        // Check if both height and weight are greater than zero
+        if (height > 0 && weight > 0) {
+            // Calculate BMI
+            const bmiValue = (weight / Math.pow(height / 100, 2)).toFixed(1);
+            setBMI(bmiValue);
+        }
+    }, [height, weight]);
+
+    const getBMIClass = () => {
+        const bmiValue = bmi;
+        if (bmiValue < 18.5) {
+            return "Underweight";
+        } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+            return "Normal";
+        } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+
     return (
-        <>
-            <Card className="bg-dark opacity-75">
+      
+        <Card className="bg-dark opacity-75">
+            <Stack direction="horizontal" gap={4}>
+                
+                <Card.Title className="text-white"> BMI Calculator </Card.Title>
+                
+                
+            </Stack>   
+            <Card.Body>
                 <Stack direction="horizontal" gap={4}>
                     <Stack gap={4}>
-                    <Card.Title className="text-white"> BMI Calculator </Card.Title>
+                        <Card.Body className="height-card" style={{width:"300px"}}>
+                            <Stack direction="horizontal" gap={4}>
+                                
+                                <Card.Body>Height:</Card.Body>
+                            
+                                <TextField
+                                    id="height"
+                                    label="in cm"
+                                    type="number"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    value={height}
+                                    onChange={(e) => setHeight(e.target.value)}
+                                />
+                                
+                            </Stack>   
+                        </Card.Body>  
+
+                        <Card.Body className="weight-card" style={{width:"300px"}}>
+                            <Stack direction="horizontal" gap={4}>
+                                
+                                    <Card.Body>Weight:</Card.Body>
+                                
+                                    <TextField
+                                    id="weight"
+                                    label="in kg"
+                                    type="number"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    value={weight}
+                                    onChange={(e) => setWeight(e.target.value)}
+                                />
+                                
+                            </Stack>   
+                        </Card.Body>
                     </Stack>
-                    <Stack gap={4}>
-                    <DropdownButton
-                        id="dropdown-button-dark-example2"
-                        variant="secondary"
-                        menuVariant="dark"
-                        title={selected}
-                        className="mt-2"
-                        onSelect={handleSelect}>
-                        <Dropdown.Item eventKey="This Week" href="#/action-1">This Week</Dropdown.Item>
-                        <Dropdown.Item eventKey="Last Week" href="#/action-2">Last Week</Dropdown.Item>
-                        <Dropdown.Item eventKey="Last Month" href="#/action-3">Last Month</Dropdown.Item>
-                        <Dropdown.Item eventKey="Last Year" href="#/action-4">Last Year</Dropdown.Item>
-                    </DropdownButton>
+                    <Stack direction='vertical' gap={4}>
+                        <Card bg='secondary' style={{width:"300px", height:"200px"}}>
+                        <Card.Title>Body Mass Index (BMI)</Card.Title>
+                        <Card.Body className="calculator-card">
+                            <Stack direction="horizontal" gap={4}>
+                                
+                                    <Card.Body className="BMIvalue">{bmi}</Card.Body>
+                                    <Card.Body className='healthy'>You're {getBMIClass()}</Card.Body>
+                                
+                            </Stack> 
+
+                            <LinearProgress
+                                variant="determinate"
+                                height='5'
+                                value={bmi}
+                                sx={{
+                                    background: 'linear-gradient(to right, #6fcbb6, #9c64f4)',
+                                    '> span': { backgroundColor: 'red' },
+                                }}
+                                />
+                        </Card.Body>
+                        </Card>
                     </Stack>
-                </Stack>   
-                <Card.Body>
-                    <Stack direction="horizontal" gap={4}>
-                        <Stack gap={4}>
-                            <Card.Body className="height-card">
-                                <Stack direction="horizontal" gap={4}>
-                                    <Stack gap={4}>
-                                        <Card.Body>Height</Card.Body>
-                                    </Stack>
-                                    <Stack gap={4}>
-                                        <Card.Body>value</Card.Body> 
-                                    </Stack>
-                                </Stack>   
-                            </Card.Body>                        
-                            <Card.Body className="weight-card">
-                                <Stack direction="horizontal" gap={4}>
-                                    <Stack gap={4}>
-                                        <Card.Body>Weight</Card.Body>
-                                    </Stack>
-                                    <Stack gap={4}>
-                                        <Card.Body>value</Card.Body> 
-                                    </Stack>
-                                </Stack>   
-                            </Card.Body>
-                        </Stack>
-                        <Stack gap={4}>
-                            <Card bg='secondary'>
-                            <Card.Title>Body Mass Index (BMI)</Card.Title>
-                            <Card.Body className="calculator-card">
-                                <Stack direction="horizontal" gap={4}>
-                                    <Stack gap={4}>
-                                        <Card.Body>Value</Card.Body>
-                                    </Stack>
-                                    <Stack gap={4}>
-                                        <Card.Body className='healthy'>Healthy</Card.Body>
-                                    </Stack>
-                                </Stack> 
-                                <p>scale</p>  
-                            </Card.Body>
-                            </Card>
-                        </Stack>
-                    </Stack>
-                </Card.Body>
-            </Card>
-        </>
+                </Stack>
+            </Card.Body>
+        </Card>
     )
 }
