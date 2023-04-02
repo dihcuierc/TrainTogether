333eb9b0a1c841c08f45a64e1686dcd9948ea43b
Kai <kaijie00@hotmail.com>
2023-04-03 01:44:40 +0800

    Changes

---
+++ backend/src/controller/DBConnection.js
@@ -0,0 +1,353 @@
+import {collection, onSnapshot, getFirestore, addDoc, deleteDoc, 
+    doc, query, where, orderBy, serverTimestamp, getDocs, equalsTo, getDoc, updateDoc,
+    documentId, arrayRemove, arrayUnion
+} from "firebase/firestore";
+
+//init service
+const db = getFirestore()
+
+//User class
+class User{
+    constructor(id, password, firstName, lastName, username, email, mobile, Reviews, goals){
+        this.id = id
+        this.password = password
+        this.firstName = firstName
+        this.lastName = lastName
+        this.username = username
+        this.email = email
+        this.mobile = mobile
+        this.Reviews = Reviews
+        this.goals = goals
+    }
+}
+//User class converter
+const userConverter = {
+    toFirestore: (user) => {
+        return {
+            id: user.id,
+            password: user.password,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+            email: user.email,
+            mobile: user.mobile,
+            Reviews: user.Reviews,
+            goals: user.goals
+            };
+    },
+    fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options);
+        return new User(data.id, data.password, data.firstName, data.lastName, data.username, data.email, data.mobile, data.Reviews, data.goals);
+    }
+}
+async function collectUserData() {
+    const UserRef = collection(db, 'User')
+    const reviewRef = collection(db, "Review")
+    let User = []
+    let UserRev = []
+    // real time User collection data
+    onSnapshot(UserRef, (snapshot) => {
+        snapshot.docs.forEach((Doc) => {
+            User.push({...Doc.data(), id: Doc.id})
+            const reviews = Doc.data().reviews
+            //for each user, get/display their reviews
+            for(var key in reviews) {
+                const review = doc(db, "Review", reviews[key])
+                onSnapshot(review, (doc) => {
+                    UserRev.push({...doc.data(), id: doc.id})
+                    console.log(UserRev)
+                })
+            }
+        })
+        console.log(User)
+        return UserRev
+    })
+    return User
+}
+
+function collectOneUser(id){
+    // id represents a User's auto-generated id
+    const UserRef = doc(db, 'User', id)    
+
+    let User = []
+    onSnapshot(UserRef, (doc) => {
+        User.push({...doc.data(), id: doc.id})
+        console.log("User data: ", User)
+    })
+    return User
+}
+
+async function addUserData(new_password, new_firstName, new_lastName, new_username, new_email, new_image, new_mobile) {
+    // add User data
+    const UserRef = collection(db, "User");
+    const data = {
+        //all the attributes
+        password: new_password,
+        firstName: new_firstName,
+        lastName: new_lastName,
+        username: new_username,
+        email: new_email,
+        image: new_image,
+        mobile: new_mobile,
+        //goals, Reviews, friends can add separately
+        createdAt: serverTimestamp()
+    }
+    const ref = await addDoc(UserRef, data)
+    .then(() => {
+        console.log("User has been added successfully");
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    return "User has been added successfully"
+}
+
+function deleteUserData(docID) {
+    // delete User data using document's ID
+    const UserRef = doc(db, "User", docID)
+
+    deleteDoc(UserRef)
+    .then(() => {
+        console.log("User has been deleted successfully.")
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    return "User has been deleted successfully"
+}
+
+function collectRevData(){
+    //Review collection ref
+    const reviewRef = collection(db, 'Review')
+    let Review = []
+    // real time Review collection data
+    onSnapshot(reviewRef, (snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            Review.push({...doc.data(), id: doc.id})
+        })
+        console.log(Review)
+    })
+    return Review
+};
+
+function collectOneRev(id){
+    // id represents the auto-generated ID
+    const reviewRef = collection(db, 'Review')
+    const review = query(reviewRef, where("UID", "==", id))
+    
+    // collect all reviews from one user
+    onSnapshot(review, (snapshot) => {
+        let userRev = []
+        snapshot.docs.forEach((doc) => {
+            userRev.push({...doc.data(), id: doc.id})
+        })
+        console.log(userRev)
+    })
+    return userRev
+}
+
+async function addRevData(new_UID, new_comment, new_name, new_rating, new_exID){
+    // add Review data
+    const reviewRef = collection(db, "Review");
+    const UserRef = doc(db, "User", new_UID)
+    const data = {
+        //all the attributes
+        UID: new_UID,
+        comment: new_comment,
+        name: new_name,
+        rating: new_rating,
+        exID: new_exID,
+        createdAt: serverTimestamp()
+    }
+    addDoc(reviewRef, data)
+    .then(async (item) => {
+        await updateDoc(UserRef, {
+            reviews: arrayUnion(item.id)
+        });
+        console.log("Review has been added successfully");
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    return "Review has been added successfully"
+}
+
+async function deleteRevData(UID, revID){
+    // delete Review data
+    //const reviewRef = collection(db, "Review")
+    //const review = query(reviewRef, where("UID", "==", id), where("exID", "==", exID))
+    const UserRef = doc(db, "User", UID)
+    const review = doc(db, "Review", revID)
+
+    await deleteDoc(review)
+    .then(async () => {
+        await updateDoc(UserRef, {
+            reviews: arrayRemove(revID)
+        });
+        console.log("Review has been deleted successfully");
+    })
+    return "Review has been deleted successfully"
+}
+
+function collectGoalData(){
+    const goalRef = collection(db, 'Goal')
+
+    onSnapshot(goalRef, (snapshot) => {
+        let Goal = []
+        snapshot.docs.forEach((doc) => {
+            Goal.push({...doc.data(), id: doc.id})
+        })
+        console.log(Goal)
+    })
+    return Goal
+}
+
+function collectOneGoal(id){
+    // id represents a User's unqiue id
+    const goalRef = collection(db, 'Goal')
+    const goal = query(goalRef, where("UID", "==", id))
+    
+    // collect all goals from one user
+    onSnapshot(goal, (snapshot) => {
+        let userGoal = []
+        snapshot.docs.forEach((doc) => {
+            userGoal.push({...doc.data(), id: doc.id})
+        })
+        console.log(userGoal)
+    })
+    return userGoal
+}
+
+async function addGoalData(new_UID, new_Title, new_CV, new_TV, new_image, new_Deadline, new_Done){
+    // add Goal data
+    const goalRef = collection(db, "Goal");
+    const UserRef = doc(db, "User", new_UID)
+    const data = {
+        //all the attributes
+        UID: new_UID,
+        Title: new_Title,
+        CurrentValue: new_CV,
+        TargetValue: new_TV,
+        Deadline: new_Deadline,
+        Done: new_Done,
+        image: new_image,
+        createdAt: serverTimestamp()
+    }
+    addDoc(goalRef, data)
+    .then(async (item) => {
+        await updateDoc(UserRef, {
+            goals: arrayUnion(item.id)
+        });
+        console.log("Goal has been added successfully");
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    return "Goal has been added successfully"
+}
+
+function deleteGoalData(UID, GID){
+    // delete Goal data
+    const goalRef = collection(db, "Goal")
+    const goal = query(goalRef, where("UID", "==", UID), where("GID", "==", GID))
+
+    getDocs(goal).then(function(snapshot) {
+        snapshot.forEach(function(doc){
+            deleteDoc(doc.ref)
+        })
+        console.log("Goal has been deleted successfully")
+    })
+    return "Goal has been deleted successfully"
+}
+
+function collectExData(){
+    const ExRef = collection(db, 'Exercise')
+    onSnapshot(ExRef, (snapshot) => {
+        let Exercise = []
+        snapshot.docs.forEach((doc) => {
+            Exercise.push({...doc.data(), id: doc.id})
+        })
+        console.log(Exercise)
+    })
+}
+
+//Collect data of one exercise only
+function collectOneEx(exID){
+    // id represents an exercise's unqiue id
+    const ExRef = collection(db, 'Exercise')
+    const exercise = query(ExRef, where("exID", "=", exID))
+    
+    // collect one exercise only
+    onSnapshot(exercise, (doc) => {
+        console.log(doc.data(), doc.id)
+    })
+}
+
+
+function addExData(){
+    // add new exercise data
+    const goalRef = collection(db, "Exercise");
+    const data = {
+        //all the attributes
+        exID: new_exID,
+        
+        createdAt: serverTimestamp()
+    }
+    addDoc(goalRef, data)
+    .then(() => {
+        console.log("Goal has been added successfully");
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
+function collectCH(){
+    const CHref = collection(db, "Calories History")
+}
+
+function collectOneCH(){
+
+}
+
+function addCH(){
+
+}
+
+function deleteCH(){
+
+}
+
+function collectSE(){
+
+}
+
+function collectOneSE(){
+
+}
+
+function addSE(){
+
+}
+
+function deleteSE(){
+
+}
+
+function collectExGroup(){
+
+}
+
+function collectOneExGroup(){
+
+}
+
+function addExGroup(){
+
+}
+
+function deleteExGroup(){
+
+}
+export {collectUserData, collectOneUser, addUserData, deleteUserData, collectRevData, collectOneRev, addRevData, deleteRevData,
+    collectGoalData, collectOneGoal, addGoalData, deleteGoalData};
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,34 @@ import express from "express";
 import cors from "cors";
 
 import authRoutes from "./routes/authentication";
-import {collectUserData} from "./DBConnection";
+import {addUserData, collectOneRev, collectOneUser, collectRevData, collectUserData, deleteUserData, addRevData, deleteRevData, collectGoalData,
+collectOneGoal, addGoalData, deleteGoalData} from "./controller/DBConnection";
+import userRoutes from "./routes/user";
 import {errorHandler} from "./errors/errorHandler";
+import { deleteUser } from "firebase/auth";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/users",authRoutes);
+app.use("/DB", userRoutes)
 app.use(errorHandler);
 
 app.listen(8080, () => {
     console.log(`Server running on localhost:8080`);
-    collectUserData();
+    //collectUserData();
+    collectRevData();
+    //collectOneUser("9hkil8WTIIWf2XJdZxWU");
+    //collectOneRev("9hkil8WTIIWf2XJdZxWU");
+    //addUserData("123456", "Kai Jie", "Wan", "kjwan", "kjwan@gmail.com", null, "98765432")
+    //deleteUserData("fTF3x1lyCfxvQJbepxRi")
+    //collectOneRev("9hkil8WTIIWf2XJdZxWU")
+    //addRevData("9hkil8WTIIWf2XJdZxWU", "Excellent workout", "Jojo", 4.8, 1)
+    //deleteRevData("9hkil8WTIIWf2XJdZxWU", "di5dGgNI0nhIhDCmQTwG")
+    //collectGoalData()
+    //collectOneGoal("9hkil8WTIIWf2XJdZxWU")
+    //addGoalData("9hkil8WTIIWf2XJdZxWU", "Gain muscle mass", 62, 75, "", "20-5-23", false)
+    //deleteGoalData("9hkil8WTIIWf2XJdZxWU", 2)
+    
 })
