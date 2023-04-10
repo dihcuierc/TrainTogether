d98eae52d7dc2714a81e7c594d789da32e119200
edmund <edmundtlz@gmail.com>
2023-04-10 20:50:59 +0800

    fix styling

--- app/src/pages/facilities/ExerciseFacilities/Facilities.js
+++ app/src/pages/facilities/ExerciseFacilities/Facilities.js
@@ -29,37 +29,39 @@ export default function Facilities() {
                             <h3 className="d-flex justify-content-center text-black-50 mb-1">Park Details</h3>
                         </div>
                         {details !== null &&
-                        <Card.Body className="pt-0">
+                        <Card.Body className="p-5">
                             <Card.Title className="display-6">
                                 {details.Name}
                             </Card.Title>
                             <Card.Subtitle className="small ms-2 text-black-50">
                                 {details.ADDRESSSTREETNAME}
                             </Card.Subtitle>
-                            <div className="d-flex pt-2">
-                                <MapIcon color="success" fontSize="large" className="me-3"/>
-                                <Card.Text className="my-auto">
-                                    {details.ADDRESSPOSTALCODE}
-                                </Card.Text>
-                            </div>
-                            <div className="d-flex">
-                                <TimeIcon color="primary" fontSize="large" className="me-3"/>
-                                <div className="d-flex mt-1">
-                                    {details.OpeningHours === "24h" ? <Card.Text className="text-success me-2">Open</Card.Text> : <Card.Text className="text-danger me-2">Closed</Card.Text>}
-                                    {details.OpeningHours}
+                            <div>
+                                <div className="d-flex pt-4">
+                                    <MapIcon color="success" fontSize="large" className="me-3"/>
+                                    <Card.Text className="my-auto">
+                                        {details.ADDRESSPOSTALCODE}
+                                    </Card.Text>
                                 </div>
-                            </div>
-                            <div className="d-flex">
-                                <WebIcon color="secondary" fontSize="large" className="me-3"/>
-                                <Card.Text className="my-auto">
-                                    <Link to={details.HYPERLINK} className={`${textStyle.hoverline}`}> NPark Website</Link>
-                                </Card.Text>
-                            </div>
-                            <div className="mt-5">
-                                {details.TAG.map((tag) => (
-                                    <Badge bg="primary" className="me-1">{tag}</Badge>
-                                ))
-                                }
+                                <div className="d-flex">
+                                    <TimeIcon color="primary" fontSize="large" className="me-3"/>
+                                    <div className="d-flex mt-1">
+                                        {details.OpeningHours === "24h" ? <Card.Text className="text-success me-2">Open</Card.Text> : <Card.Text className="text-danger me-2">Closed</Card.Text>}
+                                        {details.OpeningHours}
+                                    </div>
+                                </div>
+                                <div className="d-flex">
+                                    <WebIcon color="secondary" fontSize="large" className="me-3"/>
+                                    <Card.Text className="my-auto">
+                                        <Link to={details.HYPERLINK} className={`${textStyle.hoverline}`}> NPark Website</Link>
+                                    </Card.Text>
+                                </div>
+                                <div className="mt-5">
+                                    {details.TAG.map((tag) => (
+                                        <Badge bg="primary" className="me-1">{tag}</Badge>
+                                    ))
+                                    }
+                                </div>
                             </div>
                         </Card.Body>
                         }
